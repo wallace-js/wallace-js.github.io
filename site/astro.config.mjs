4b0aaa742ceae6a94cbd4241a51f727f2d4c7bd5
Andrew Buchan <andyhasit@gmail.com>
2026-01-09 00:44:53 +0000

    Attempting to install starlight

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://wallace-js.github.io",
   base: "/wallace/",
+  trailingSlash: "ignore",
+  integrations: [
+    starlight({
+      title: "Wallace",
+      // sidebar: [],
+    }),
+  ],
 });
